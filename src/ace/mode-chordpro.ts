28371dbe2456899972d31fcea5c6203d669d3a3e
Glowstudent777 <cameronmenken7@gmail.com>
2024-02-24 23:36:39 -0600

    Chord highlighting

---
+++ src/ace/mode-chordpro.ts
@@ -0,0 +1,121 @@
+import ace from "ace-builds/src-noconflict/ace";
+import "ace-builds/src-noconflict/mode-text";
+
+export class ChordProHighlightRules extends ace.require("ace/mode/text_highlight_rules").TextHighlightRules {
+    constructor() {
+        super();
+
+        const reOpenBrace = '(^\\s*{)';
+        const reCloseBrace = '(\\s*}\\s*$)';
+        const reColon = '(\\s*:)';
+
+        const reNumber = '\\b[0-9]+\\b';
+
+        const tkBrace = 'meta.tag';
+        const tkCommand = 'meta';
+        const tkSingleTag = 'entity.name';
+
+        this.$rules = {
+            start: [{
+                token: 'comment',
+                regex: '^#.*$', // debated this, for now MUST be first character (otherwise allow \\s*)
+            }, {
+                token: [tkBrace, tkSingleTag, tkBrace],
+                regex: `${reOpenBrace}(column_break|new_page|np|colb|start_of_chorus|soc|end_of_chorus|eoc)${reCloseBrace}`,
+                caseInsensitive: true,
+            }, {
+                token: [tkBrace, tkSingleTag, tkBrace],
+                regex: `${reOpenBrace}(start_of_tab|sot)${reCloseBrace}`,
+                caseInsensitive: true,
+                next: 'tabBlockTag',
+            }, {
+                token: [tkBrace, tkCommand, tkBrace],
+                regex: `${reOpenBrace}(define)${reColon}`,
+                caseInsensitive: true,
+                next: 'defineTag',
+            }, {
+                // tkCommand?
+                token: [tkBrace, 'meta', tkBrace, 'text', tkBrace],
+                regex: `${reOpenBrace}(c|comment)${reColon}(.*)${reCloseBrace}`,
+                caseInsensitive: true,
+            }, {
+                token: [tkBrace, tkCommand, tkBrace, 'string', tkBrace],
+                regex: `${reOpenBrace}(title|t|subtitle|st|artist|album|instrument|tuning|key|k)${reColon}(.*)${reCloseBrace}`,
+                caseInsensitive: true,
+            }, {
+                token: [tkBrace, 'invalid', tkBrace, 'string', tkBrace],
+                regex: `${reOpenBrace}([-\\S]+)${reColon}(.*)${reCloseBrace}`,
+                caseInsensitive: true,
+            }, {
+                token: [tkBrace, 'invalid', tkBrace],
+                regex: `${reOpenBrace}(.+)${reCloseBrace}`,
+                caseInsensitive: true,
+            }, {
+                token: 'constant.numeric',
+                regex: reNumber,
+            }, {
+                token: ['constant.character.escape', 'keyword', 'constant.character.escape'],
+                regex: '(\\[)(.*?)(\\])',
+            }, {
+                token: 'text',
+                regex: '\\s+',
+            }],
+
+            defineTag: [{
+                token: tkBrace,
+                regex: reCloseBrace,
+                next: 'start',
+            }, {
+                token: 'constant.mumeric',
+                regex: reNumber,
+            }, {
+                token: 'keyword.control',
+                regex: '\\b(fingers|frets|finger|fret|string)\\b',
+                caseInsensitive: true,
+            }, {
+                token: [tkCommand, tkBrace],
+                regex: `\\b(add)${reColon}`,
+                caseInsensitive: true,
+            }, {
+                defaultToken: 'string',
+            }],
+
+            tabBlockTag: [{
+                token: [tkBrace, tkSingleTag, tkBrace],
+                regex: `${reOpenBrace}(end_of_tab|eot)${reCloseBrace}`,
+                caseInsensitive: true,
+                next: 'start',
+            }, {
+                token: 'comment.line',
+                regex: '-+',
+            }, {
+                token: 'constant.character',
+                regex: '\\|+',
+            }, {
+                token: 'string',
+                regex: '[a-gA-G][b#]?',
+            }, {
+                token: 'constant.mumeric',
+                regex: reNumber,
+            }, {
+                defaultToken: 'comment',
+            }],
+        };
+
+        this.normalizeRules();
+
+        this.$rules.start.unshift({
+            token: 'string',
+            regex: '\\[([A-G][b#]?(m|dim|aug|sus2|sus4|add9|7|9|11|13|\\+|\\-|\\(b5\\)|\\(b9\\)|\\(b13\\)|\\(b5b9\\)|\\(b5b13\\)|\\(b9b13\\)|\\(b5b9b13\\))?)\\]',
+        });
+
+    };
+}
+
+export default class ChordProMode extends ace.require("ace/mode/text").Mode {
+    constructor() {
+        super();
+
+        this.HighlightRules = ChordProHighlightRules;
+    }
+}
--- src/components/chordProEditor.tsx
+++ src/components/chordProEditor.tsx
@@ -1,10 +1,18 @@
-import AceEditor from 'react-ace';
-import "ace-builds/src-noconflict/theme-github";
-import "ace-builds/src-noconflict/ext-language_tools";
-import { useState } from 'react';
-import ace from 'react-ace';
+"use client"
 
-// Regex for chordpro so far: \[[^\]]*\]
+import React, { useState } from 'react';
+import AceEditor from 'react-ace'
+
+import "ace-builds/src-noconflict/mode-text";
+import "ace-builds/esm-resolver";
+import "ace-builds/src-noconflict/ext-language_tools";
+import "ace-builds/src-noconflict/ext-beautify";
+import "ace-builds/src-noconflict/ext-settings_menu";
+import "ace-builds/src-noconflict/ext-keybinding_menu";
+import "ace-builds/src-noconflict/ext-error_marker";
+
+import 'brace/theme/github'
+import ChordProMode from '@/ace/mode-chordpro';
 
 export function ChordProEditor({ onChange }: any) {
     const [editorValue, setEditorValue] = useState('');
@@ -13,13 +21,16 @@ export function ChordProEditor({ onChange }: any) {
         setEditorValue(value);
         onChange(value);
     };
+    const customMode = new ChordProMode();
+
+
     return (
         <>
             <AceEditor
-                mode="ace/mode/custom"
+                mode={customMode}
                 theme="github"
                 onChange={handleEditorChange}
-                name="chordpro-editor"
+                name="editor"
                 editorProps={{ $blockScrolling: true }}
                 setOptions={{ useWorker: false }}
                 value={editorValue}
